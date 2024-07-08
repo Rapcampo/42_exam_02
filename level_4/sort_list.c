888f64c83faeabe4fb82d67036bff1a68fb40a38
Rapcampo <raphael.campospt@gmail.com>
2024-07-08 23:26:50 +0100

    lovely sort list

---
+++ level_4/sort_list.c
@@ -0,0 +1,18 @@
+#include "list.h"
+
+t_list *sort_list(t_list *lst, int (*cmp)(int, int))
+{
+	t_list *tmp;
+	while(lst->next)
+	{
+		if ((cmp)(lst->data, lst->next->data) == 0)
+		{
+			lst->data ^= lst->next->data ^= lst->data ^= lst->next->data;
+			lst = tmp;
+		}
+		else 
+			lst = lst->next;
+	}
+	lst = tmp;
+	return (lst);
+}
